348286f5236e1eedcb3a4254b8cd4f072cef9a26
Sarah Radzihovsky <sarahradz@dn0a2437e0.sunet>
2016-05-13 12:24:35 -0700

    Skeleton

---
+++ FinalProject/PhotoCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.swift
+//  FinalProject
+//
+//  Created by Sarah Radzihovsky on 5/13/16.
+//  Copyright © 2016 Sarah Radzihovsky. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ FinalProject/TravelHistoryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TravelHistoryTableViewCell.swift
+//  FinalProject
+//
+//  Created by Sarah Radzihovsky on 5/13/16.
+//  Copyright © 2016 Sarah Radzihovsky. All rights reserved.
+//
+
+import UIKit
+
+class TravelHistoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

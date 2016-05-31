84b8b6ac9afefde66f41113798eb45066371136f
Sarah Radzihovsky <sarahradz@rescomp-15-300320.stanford.edu>
2016-05-31 02:47:41 -0700

    new Location tab done

---
+++ FinalProject/BrowseMapViewController.swift
@@ -0,0 +1,126 @@
+//
+//  BrowseMapViewController.swift
+//  FinalProject
+//
+//  Created by Sarah Radzihovsky on 5/30/16.
+//  Copyright © 2016 Sarah Radzihovsky. All rights reserved.
+//
+
+//followed tutorial from http://www.thorntech.com/2016/01/how-to-search-for-location-using-apples-mapkit/ to set up this view
+
+import UIKit
+import MapKit
+
+protocol HandleMapSearch {
+    func dropPinZoomIn(placemark: MKPlacemark)
+}
+
+class BrowseMapViewController: UIViewController {
+    
+    var resultSearchController:UISearchController? = nil
+    let locationManager = CLLocationManager()
+
+    var selectedPin:MKPlacemark? = nil
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest //overrides the default accuracy level with explicit value
+        locationManager.requestWhenInUseAuthorization() //asks user for location permission dialog
+        locationManager.requestLocation() //one time location request
+        
+        //serves as a searchResultsUpdater delegate
+        let locationSearchTable = storyboard!.instantiateViewControllerWithIdentifier("SearchForLocation") as! SearchForLocationTableViewController
+        resultSearchController = UISearchController(searchResultsController: locationSearchTable)
+        resultSearchController?.searchResultsUpdater = locationSearchTable
+        
+        //configures the search var within a navigation bar
+        let searchBar = resultSearchController!.searchBar
+        searchBar.sizeToFit()
+        searchBar.placeholder = "Search for a location"
+        navigationItem.titleView = resultSearchController?.searchBar
+        
+        //reconfigures UISearchController view
+        resultSearchController?.hidesNavigationBarDuringPresentation = false
+        resultSearchController?.dimsBackgroundDuringPresentation = true
+        definesPresentationContext = true
+        
+        //passes along handle of mapView to the SearchforLocationVC
+        locationSearchTable.mapView = mapView
+        locationSearchTable.handleMapSearchDelegate = self
+        
+    }
+    
+    func getDirections(){
+        if let selectedPin = selectedPin {
+            let mapItem = MKMapItem(placemark: selectedPin)
+            let launchOptions = [MKLaunchOptionsDirectionsModeKey : MKLaunchOptionsDirectionsModeDriving]
+            mapItem.openInMapsWithLaunchOptions(launchOptions)
+        }
+    }
+}
+
+extension BrowseMapViewController : CLLocationManagerDelegate {
+    //If user chooses "allow", we now have location permission
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if status == .AuthorizedWhenInUse {
+            locationManager.requestLocation()
+        }
+    }
+    
+    //gets called when location info comes back with an array of locations (we focus on the first item only)
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            let span = MKCoordinateSpanMake(0.05, 0.05)
+            let region = MKCoordinateRegion(center: location.coordinate, span: span)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    
+    //silences locationManagerError
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print("error:: \(error)")
+    }
+}
+
+extension BrowseMapViewController : HandleMapSearch {
+    func dropPinZoomIn(placemark: MKPlacemark) {
+        // cache the pin
+        selectedPin = placemark
+        //clear existing placemart
+        mapView.removeAnnotations(mapView.annotations)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = placemark.coordinate
+        annotation.title = placemark.name
+        if let city = placemark.locality,
+        let state = placemark.administrativeArea {
+            annotation.subtitle = "\(city) \(state)"
+        }
+        //adds annotation to map
+        mapView.addAnnotation(annotation)
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegionMake(placemark.coordinate, span)
+        mapView.setRegion(region, animated: true)
+    }
+}
+
+extension BrowseMapViewController : MKMapViewDelegate {
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView?{
+        if annotation is MKUserLocation {
+            return nil
+        }
+        let reuseId = "pin"
+        var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId) as? MKPinAnnotationView
+        pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+        pinView?.pinTintColor = UIColor.orangeColor()
+        pinView?.canShowCallout = true
+        let smallSquare = CGSize(width: 30, height: 30)
+        let button = UIButton(frame: CGRect(origin: CGPointZero, size: smallSquare))
+        button.setBackgroundImage(UIImage(named: "car"), forState: .Normal)
+        button.addTarget(self, action: "getDirection", forControlEvents: .TouchUpInside)
+        pinView?.leftCalloutAccessoryView = button
+        return pinView
+    }
+}
